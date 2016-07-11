de5a4a39008c7bb67609ee5f0ba473a2c49dc732
z583240553 <583240553@qq.com>
2016-07-11 20:24:09 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -661,7 +661,7 @@ function _M.decode(payload)
               packet[ crane_state[39] ] = bit.lshift(getnumber(38),8)+getnumber(39)   --主钩状态-变频器状态
               packet[ crane_state[43] ] = bit.lshift(getnumber(40),8)+getnumber(41)    --主钩状态-故障代码  
             end
-            --[[
+            
             --通过起升状态判断运行方向和运行速度
             if liftstate==1 or liftstate==2 then  
                 packet[ crane_state[36] ] = 1
@@ -689,7 +689,7 @@ function _M.decode(payload)
             else
               packet[ crane_state[42] ] = 1
             end
-            
+            --[[
             if packet["cranetype"]>0 then
                 local viceliftstate = bit.lshift(getnumber(62),8)+getnumber(63)                    --副起升机构状态
                 if viceliftstate>0 and viceliftstate<5 then
